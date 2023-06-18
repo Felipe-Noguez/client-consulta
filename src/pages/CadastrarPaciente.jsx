98a056727e19d077349899f6e678874855cdd0ac
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-06-18 19:24:59 -0300

    Feat: Cadastrar Paciente

--- src/pages/CadastrarPaciente.jsx
+++ src/pages/CadastrarPaciente.jsx
@@ -1,7 +1,37 @@
+import axios from "axios";
+import {useState} from "react";
+
 export const CadastrarPaciente = () => {
+    const [nome, setNome] = useState('')
+    const [telefone, setTelefone] = useState('')
+
+    async function handleSubmit() {
+        const dadosPaciente = {
+            nome: nome,
+            telefone: telefone
+        }
+        axios.post('http://localhost:8080/paciente/cadastrar-paciente', dadosPaciente)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        console.log(nome)
+        console.log(telefone)
+    }
+
     return(
         <div>
-            Cadastrar Paciente
+            <h2>Cadastrar Paciente</h2>
+            <form style={{display:'flex', flexDirection:'column', gap: 16}}>
+                <label>Nome</label>
+                <input type="text" name="paciente" value={nome} onChange={event => setNome(event.target.value)}/>
+                <label>Telefone</label>
+                <input type="text" name="telefone" value={telefone}
+                       onChange={event => setTelefone(event.target.value)}/>
+                <button type="button" onClick={handleSubmit}>Cadastrar Paciente</button>
+            </form>
         </div>
     )
 }
